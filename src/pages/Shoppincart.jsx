fe07c066a6e208666ca555b98a4ebe7ef4915fca
Aningwu chisom <152747011+SommyTechFx@users.noreply.github.com>
2024-07-08 14:32:58 +0100

    working on the shopping cart

--- src/pages/Shoppincart.jsx
+++ src/pages/Shoppincart.jsx
@@ -2,6 +2,8 @@ import { MdOutlineArrowRight } from "react-icons/md";
 import "./../components/Styles/shoppingcart.css";
 import { IoIosArrowRoundBack } from "react-icons/io";
 import { Link } from "react-router-dom";
+import { CiTrash } from "react-icons/ci";
+import pic6 from "./../images/productimages/pic6.png";
 
 function Shoppincart() {
   return (
@@ -20,13 +22,41 @@ function Shoppincart() {
           </Link>
         </div>
       </section>
-      <div id="checking-out">
-        <h3>Product</h3>
-        <h3>Price</h3>
+      <section id="product-price-quantity">
+        <div>
+          <div id="checking-out">
+            <h3>Product</h3>
+            <h3>Price</h3>
 
-        <h3>Quantity</h3>
-      </div>
-      <hr />
+            <h3>Quantity</h3>
+          </div>
+
+          <section id="add-to-cart">
+            <div id="add-to-cart-img">
+              <img src={pic6} alt="" />
+              <p>Diamond cut Ring</p>
+            </div>
+            <p>$2,500</p>
+            <div id="add-cart-calculations">
+              <button>-&nbsp; 1&nbsp; +</button>
+              <CiTrash
+                style={{
+                  color: "rgba(243, 89, 89, 1)",
+                  fontSize: "2rem",
+                  alignItems: "center",
+                }}
+              />
+            </div>
+          </section>
+        </div>
+
+        <section id="price-list">
+          <div id="each-price">
+            <h3>Subtotal</h3>
+            <p>$2,500.00</p>
+          </div>
+        </section>
+      </section>
     </section>
   );
 }
